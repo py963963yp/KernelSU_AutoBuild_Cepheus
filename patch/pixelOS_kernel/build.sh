6ebfcb813d057597f2d7fc299dd23b591bd39a0f
py963963yp <py963963yp@gmail.com>
2026-07-26 19:00:18 +0800

    更新 build.sh

--- patch/pixelOS_kernel/build.sh
+++ patch/pixelOS_kernel/build.sh
@@ -28,10 +28,10 @@ rm -rf $KERNEL_PATH/out/ *.zip
 # make mrproper && git reset --hard HEAD
 make mrproper
 
-curl -LSs "https://raw.githubusercontent.com/py963963yp/SukiSU-Ultra/main/kernel/setup.sh" | bash -s builtin
-#curl -LSs "https://raw.githubusercontent.com/ReSukiSU/ReSukiSU/main/kernel/setup.sh" | bash
+#curl -LSs "https://raw.githubusercontent.com/py963963yp/SukiSU-Ultra/main/kernel/setup.sh" | bash -s builtin
+curl -LSs "https://raw.githubusercontent.com/ReSukiSU/py963963yp/main/kernel/setup.sh" | bash
 
-#sed -i 's/type_val_to_struct/type_val_to_struct_array/g' KernelSU/kernel/selinux/sepolicy.c
+sed -i 's/type_val_to_struct/type_val_to_struct_array/g' KernelSU/kernel/selinux/sepolicy.c
 
 echo "=========================Build========================="
 make O=out cepheus_defconfig
